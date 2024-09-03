2e9acaf4d638cb96aa41e57b836dc09a1acff91f
ThEditor <theditormcpe@gmail.com>
2024-09-03 18:26:29 +0530

    fix: head title

--- frontend/src/ts/utils/misc.ts
+++ frontend/src/ts/utils/misc.ts
@@ -666,7 +666,7 @@ export function updateTitle(title?: string): void {
 
   if (title === undefined || title === "") {
     document.title =
-      local + "Monkeytype | A minimalistic, customizable typing test";
+      local + "Lugtype | A minimalistic, customizable typing test";
   } else {
     document.title = local + title;
   }
